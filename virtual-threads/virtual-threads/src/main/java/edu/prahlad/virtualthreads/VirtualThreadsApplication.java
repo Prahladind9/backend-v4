c53123575ef56452b2929a249165b2e6d16fc713
Prahlad Rao <prahladskingdom@hotmail.com>
2026-06-30 10:25:21 +0200

    Initial commit

---
+++ virtual-threads/virtual-threads/src/main/java/edu/prahlad/virtualthreads/VirtualThreadsApplication.java
@@ -0,0 +1,13 @@
+package edu.prahlad.virtualthreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualThreadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualThreadsApplication.class, args);
+    }
+
+}
